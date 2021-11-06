7ee1357f180f335b967fb055e4d24a68aa852efc
Andrew Holway <andrew.holway@gmail.com>
2021-11-06 15:04:21 +0100

    now with cors

--- hello.py
+++ hello.py
@@ -3,7 +3,9 @@ import math
 from flask import Flask
 from flask import jsonify
 from flask import render_template
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello():
